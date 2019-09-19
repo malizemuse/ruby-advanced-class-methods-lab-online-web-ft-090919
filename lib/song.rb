037278bbf094efd57ce067cda15a5c65f37f71ec
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 12:58:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,8 @@ class Song
   def self.find_or_create_by_name(song_name)
     self.find_by_name(song_name) || self.create_by_name(song_name)
   end
-
+  
+  
   def self.alphabetical
     @@all.sort_by! { |song| song.name }
   end
