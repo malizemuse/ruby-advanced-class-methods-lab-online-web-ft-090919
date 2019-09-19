eeb81fc4ab0d4d952d09b30c0cdc0a08f5acceee
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 12:56:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
     new_song
   end
   
-  # Creates new obj; Sets obj name; 
+  # Creates new obj; Sets obj name; Returns new obj
   def self.new_by_name(song_name)
     new_song = self.new
     new_song.name = song_name
