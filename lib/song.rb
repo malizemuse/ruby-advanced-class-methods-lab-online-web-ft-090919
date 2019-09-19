51055f50129da2bda066e2ac7be4d62ffda6b855
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 12:52:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,7 @@
 class Song
-  
-  @@all = []
-  
   attr_accessor :name, :artist_name
-  
+  @@all = []
+
   def self.all
     @@all
   end
@@ -11,42 +9,73 @@ class Song
   def save
     self.class.all << self
   end
-  
-  def self.create 
-    song = self.new 
-    song.save
-    song 
-  end 
-  
-  def self.new_by_name(name)
-    song = self.new
-    song.name = name
-    song 
-  end 
-  
-  def self.create_by_name(name)
-    song = self.new 
-    song.name = name 
-    song.save 
-    song
-  end 
-  
-  def self.find_by_name(name)
-    @@all.find { |song| song.name == name }
-  end 
-  
-  def find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
-  end 
-  
-  
+
+  def self.create
+    new_song = self.new
+    @@all << new_song
+    new_song
+  end
+
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+  end
+
+  def self.create_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    @@all << new_song
+    new_song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect { |song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by! { |song| song.name }
+  end
+
+  def self.new_from_filename(file_name)
+    new_song = self.new
+    new_song.name = file_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file_name.split(" - ")[0]
+    new_song
+  end
+
+  def self.create_from_filename(file_name)
+    new_song = self.new
+    new_song.name = file_name.split(" - ")[1].split(".")[0]
+    new_song.artist_name = file_name.split(" - ")[0]
+    @@all << new_song
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 end
 
 
 # TEST CODE 
-
 song = Song.create
+Song.all.include?(song)
 
-puts Song.all.include?(song)
+song = Song.new_by_name("Mmmbop")
+song.name
 
+song = Song.create_by_name("Mmmbop")
+song
+Song.all.include?(song)
 
+mmmbop = Song.create_by_name("Mmmbop")
+
+song = Song.new_from_filename("Kanye West - Workout Plan.mp3")
+song.name
+song.artist_name
+
+Song.destroy_all
