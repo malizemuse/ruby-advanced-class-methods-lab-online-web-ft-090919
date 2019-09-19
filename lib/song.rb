912eabbc7738ab2ed94dced8165c48e45f45cabd
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 13:02:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,7 @@ class Song
     @@all.sort_by! { |song| song.name }
   end
   
-  
+  # 
   def self.new_from_filename(file_name)
     new_song = self.new
     new_song.name = file_name.split(" - ")[1].split(".")[0]
