66da6ed3eb19af6697e1347370120253eec1e9a3
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-19 12:50:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 class Song
-  attr_accessor :name, :artist_name
+  
   @@all = []
-
+  
+  attr_accessor :name, :artist_name
+  
   def self.all
     @@all
   end
@@ -9,5 +11,42 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create 
+    song = self.new 
+    song.save
+    song 
+  end 
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song 
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    song.save 
+    song
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find { |song| song.name == name }
+  end 
+  
+  def find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end 
+  
+  
 end
+
+
+# TEST CODE 
+
+song = Song.create
+
+puts Song.all.include?(song)
+
+
